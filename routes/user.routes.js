0db16a36490d57eec2068be4bdcdd4792da1d9ee
Tiago Rato <tiagorato13@hotmail.com>
2023-03-13 23:54:03 +0000

    minor changes to user route

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -15,4 +15,65 @@ router.get("/user", async (req, res, next) => {
   }
 });
 
+//Get single User
+
+router.get("/user/:id", async (req, res, next) => {
+  const { id } = req.params;
+
+  try {
+    const user = await User.findById(id);
+
+    res.json(user);
+  } catch (error) {
+    res.json(error);
+  }
+});
+
+//Update
+
+router.put("/user/edit/:id", async (req, res, next) => {
+  const { id } = req.params;
+  const { name, email, password } = req.body;
+
+  //check if id is a mongoDB valid ID
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    res.json("The provided id is not valid");
+  }
+
+  try {
+    const updatedUser = await User.findByIdAndUpdate(
+      id,
+      {
+        name,
+        email,
+        password,
+      },
+      { new: true }
+    );
+
+    res.json(updatedUser);
+  } catch (error) {
+    res.json(error);
+  }
+});
+
+//Delete
+
+router.delete("/user/:id", async (req, res, next) => {
+  const { id } = req.params;
+
+  //check if id is a mongoDB valid ID
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    res.json("The provided id is not valid");
+  }
+
+  //remove the project
+  try {
+    await User.findByIdAndRemove(id);
+    res.json({ message: `User with the id ${id} deleted successfully` });
+  } catch (error) {
+    res.json(error);
+  }
+});
+
 module.exports = router;
