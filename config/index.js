e1803a65b1c30230cd3b997ba78d760f36e661a9
miacsilva <miacsilvac@hotmail.com>
2023-05-30 12:12:29 +0100

    frontend url updated

--- config/index.js
+++ config/index.js
@@ -25,7 +25,7 @@ module.exports = (app) => {
   // controls a very specific header to pass headers from the frontend
   app.use(
     cors({
-      origin: [FRONTEND_URL, 'https://marialuisasilvapsicologia.netlify.app'],
+      origin: [FRONTEND_URL, 'http://marialuisasilvapsicologia.com'],
     })
   );
 
