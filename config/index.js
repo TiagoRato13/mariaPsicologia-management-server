0a86c0ca9b2b011e11ab7c3ce030b6a7cd001bb5
miacsilva <miacsilvac@hotmail.com>
2023-05-30 11:44:29 +0100

    config index.js test

--- config/index.js
+++ config/index.js
@@ -15,6 +15,17 @@ const cors = require("cors");
 
 const FRONTEND_URL = process.env.ORIGIN || "http://localhost:3000";
 
+
+/* test____ test ____ test _____*/
+
+const app = express();
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'https://marialuisasilvapsicologia.netlify.app');
+  next();
+});
+
+
 // Middleware configuration
 module.exports = (app) => {
   // Because this will be hosted on a server that will accept requests from outside and it will be hosted ona server with a `proxy`, express needs to know that it should trust that setting.
