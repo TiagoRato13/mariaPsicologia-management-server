4ec9e873535d6e1bafce5d8390ab16aa60ea554c
Maria Carvalho <miacsilvac@hotmail.com>
2024-06-04 19:47:18 +0100

    a

--- config/index.js
+++ config/index.js
@@ -2,11 +2,9 @@
 const express = require("express");
 
 // ℹ️ Responsible for the messages you see in the terminal as requests are coming in
-// https://www.npmjs.com/package/morgan
 const logger = require("morgan");
 
 // ℹ️ Needed when we deal with cookies (we will when dealing with authentication)
-// https://www.npmjs.com/package/cookie-parser
 const cookieParser = require("cookie-parser");
 
 // ℹ️ Needed to accept requests from 'the outside'. CORS stands for cross origin resource sharing
@@ -25,15 +23,19 @@ module.exports = (app) => {
   // controls a very specific header to pass headers from the frontend
   app.use(
     cors({
-      origin: [FRONTEND_URL, 'https://marialuisasilvapsicologia.com'],
+      origin: ['*'],
     })
   );
 
   // In development environment the app logs
   app.use(logger("dev"));
 
+  console.log("start")
+
   // To have access to `body` property in the request
   app.use(express.json());
   app.use(express.urlencoded({ extended: false }));
   app.use(cookieParser());
 };
+
+
